4c53fc090c5dcdc76cda518d3c504cca52acabf7
Matheus Vargas <m6.vargas@gmail.com>
2019-11-11 12:04:56 -0300

    Refactoring 7: Replace Conditional with Polymorphism

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -6,21 +6,40 @@ public class Movie {
   
     private String _title;
     private int _priceCode;
-  
-    public Movie(String title, int priceCode) {
-        _title = title;
-        _priceCode = priceCode;
+
+    private Price _price;
+
+    public Movie(String name, int priceCode) {
+        _title = name;
+        setPriceCode(priceCode);
     }
-  
-    public int getPriceCode() {
-        return _priceCode;
-    }
-  
-    public void setPriceCode(int arg) {
-       _priceCode = arg;
-    }
-  
-    public String getTitle (){
+
+    public String getTitle () {
         return _title;
-    };
-  }
+    }
+
+    double getCharge(int daysRented) {
+        return _price.getCharge(daysRented);
+    }
+
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return _price.getFrequentRenterPoints(daysRented);
+    }
+
+}
