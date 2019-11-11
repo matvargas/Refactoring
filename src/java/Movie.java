277593b315d385ce78d19e2abc6eee18ec508a73
Matheus Vargas <m6.vargas@gmail.com>
2019-11-11 10:41:55 -0300

    Correção de projeto

--- src/java/Movie.java
+++ src/java/Movie.java
@@ -1,3 +1,5 @@
+package java;
+
 public class Movie {
 
     public static final int  CHILDRENS = 2;
--- src/java/Rental.java
+++ src/java/Rental.java
@@ -1,3 +1,5 @@
+package java;
+
 class Rental {
     private Movie _movie;
     private int _daysRented;
