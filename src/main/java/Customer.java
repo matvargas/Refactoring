6e031e4597efb6deaf27b0b859d72c517e5e0c38
Matheus Vargas <m6.vargas@gmail.com>
2019-11-11 11:27:38 -0300

    Refactoring 6: Replace Temp With Query

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -30,7 +30,6 @@ class Customer {
            // show figures for this rental
            result += "\t" + each.getMovie().getTitle()+ "\t" +
                    String.valueOf(each.getCharge()) + "\n";
-           totalAmount += each.getCharge();
        }
 
        // add footer lines
@@ -39,4 +38,24 @@ class Customer {
                + " frequent renter points";
        return result;
  }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
 }
